0b2e3b559412a2d878e4b7392be654eded8c9b88
usman maqbool <usmanmaqbool636@gmail.com>
2020-08-12 08:44:07 +0500

    build with heroku

---
+++ Client/build/precache-manifest.8fa8c5101b58b22a4fa9f51c028e9105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c70d5b283eb4e8acd3029e2f2894c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b3e72b6d460dccf69e",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "f4d5eb663ecf15c77eaf",
+    "url": "/static/css/main.0f3fad49.chunk.css"
+  },
+  {
+    "revision": "90b3e72b6d460dccf69e",
+    "url": "/static/js/2.242abe34.chunk.js"
+  },
+  {
+    "revision": "e02a8f89e7a80e968f31e78707752817",
+    "url": "/static/js/2.242abe34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d5eb663ecf15c77eaf",
+    "url": "/static/js/main.ee911894.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
